79282d248d3565e13971d90f38caa96303e44635
Ajani Davis <69487297+andavis24@users.noreply.github.com>
2025-09-22 15:23:35 -0400

    Add files via upload

---
+++ andavis24_assignment_4.py
@@ -0,0 +1,8 @@
+student_name = "Ajani"
+current_gpa = 4.0
+study_hours = 25
+social_points = 45
+stress_level = 40
+
+print(f"Welcome to college life {student_name}!")
+print(f"GPA: {current_gpa}, Study Hours: {study_hours}, Social Points: {social_points}, Stress Level: {stress_level}")
